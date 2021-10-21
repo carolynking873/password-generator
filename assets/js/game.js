57936907d0e1dde35ec6ab2fd5ccec5828e627c2
Your Majesty King <carolynking873@gmail.com>
2021-10-21 00:19:41 -0500

    initial js to create password

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!");
+var employeePassword = window.prompt("Please create a password.");
+console.log(employeePassword);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our password is " + employeePassword);
+
+// this creates a function named "generate"
+
+function generate() {
+    window.alert("A password has been generated.");
+}
+
+// generate();
