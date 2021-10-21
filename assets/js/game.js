d7aa722d4b0d2614e3817411e725401e71f886cb
Your Majesty King <carolynking873@gmail.com>
2021-10-21 18:32:20 -0500

    button

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,26 +1,5 @@
-var employeePassword = window.prompt("Please create a password.");
-var employeePasswordMaxCharacters = 128;
-var employeePasswordMinCharacters = 8;
+document.getElementById("submit").onclick = function() {generatePassword()};
 
-// You can also log multiple values at once like this
-console.log(employeePassword, employeePasswordMinCharacters, employeePasswordMaxCharacters);
-
-var generate = function() {
-    // Alert employee that they are generating a password
-    window.alert("Welcome to the Password Generator!");
-
-    // Log a resulting message to the console so we know that it worked.
-    console.log(
-        employeePassword + " created"
-    );
-
-    // check password length
-    if (employeePassword < 8) {
-        window.alert(employeePassword + " is too short.");
-    }
-    else {
-        window.alert(employeePassword + " created ");
-    }
-};
-
-generate();
+function generatePassword() {
+    document.getElementById("submit").innerHTML = "YOU CLICKED ME!";
+}
