aa8bca7ea9876ac788fde52775564ce3ebff3dc7
Your Majesty King <carolynking873@gmail.com>
2021-10-22 01:23:59 -0500

    progress

--- assets/js/game.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("submit").onclick = function() {generatePassword()};
-
-function generatePassword() {
-    var password = prompt("Please select criteria: length, lowercase, uppercase, numeric, special characters");
-    if (password != null) {
-        document.getElementById("submit").innerHTML = 
-        "You selected " + password;
-    }
-}
